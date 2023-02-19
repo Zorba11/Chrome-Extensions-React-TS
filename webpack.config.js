4aa993a93be1456144bfbadfadce550e770f97f6
ageorge <ageorge@marchnetworks.com>
2023-02-19 12:02:38 -0500

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
   },
   module: {
     rules: [
@@ -15,22 +16,26 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
       },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
-          to: path.resolve('dist/manifest.json'),
+          from: path.resolve('src/static'),
+          to: path.resolve('dist'),
         },
       ],
     }),
-    new HTMLPlugin({
-      title: 'React Extension',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
+    ...getHTMLPlugins(['popup', 'options']),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -39,4 +44,21 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
+  optimization: {
+    // allows chunks to share modules
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
+
+function getHTMLPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HTMLPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
